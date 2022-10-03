e6fd915d4df095d62e2d5431fa7aafb747aed189
Yuriy <ura.yakovlev@yandex.ru>
2022-10-03 14:22:51 +0600

    Initial Commit

---
+++ tic-tac-toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tic-tac-toe
+//
+//  Created by Юрий Яковлев on 03.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
